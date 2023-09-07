0bd88ebbaa67f035a899938bd3e93c04f1bd638e
AlonMesh <alon3400@gmail.com>
2023-09-07 14:47:19 +0300

    Supporting Type

---
+++ Pokemon/type.py
@@ -0,0 +1,183 @@
+from enum import Enum, auto
+
+
+class Type(Enum):
+    FIRE = auto()
+    WATER = auto()
+    ELECTRIC = auto()
+    GRASS = auto()
+    ICE = auto()
+    FIGHTING = auto()
+    POISON = auto()
+    GROUND = auto()
+    FLYING = auto()
+    PSYCHIC = auto()
+    BUG = auto()
+    ROCK = auto()
+    GHOST = auto()
+    DRAGON = auto()
+    DARK = auto()
+    STEEL = auto()
+    FAIRY = auto()
+    NORMAL = auto()
+
+
+def string_to_type(type_str):
+    """
+    Converts a string to a Type Enum.
+
+    Args:
+        type_str (str): The type string to convert.
+
+    Returns:
+        Type: The corresponding Type Enum value.
+
+    Raises:
+        ValueError: If the provided type string is not a valid type.
+    """
+    type_mapping = {
+        "FIRE": Type.FIRE,
+        "WATER": Type.WATER,
+        "ELECTRIC": Type.ELECTRIC,
+        "GRASS": Type.GRASS,
+        "ICE": Type.ICE,
+        "FIGHTING": Type.FIGHTING,
+        "POISON": Type.POISON,
+        "GROUND": Type.GROUND,
+        "FLYING": Type.FLYING,
+        "PSYCHIC": Type.PSYCHIC,
+        "BUG": Type.BUG,
+        "ROCK": Type.ROCK,
+        "GHOST": Type.GHOST,
+        "DRAGON": Type.DRAGON,
+        "DARK": Type.DARK,
+        "STEEL": Type.STEEL,
+        "FAIRY": Type.FAIRY,
+        "NORMAL": Type.NORMAL,
+    }
+
+    # Convert to uppercase for case-insensitivity
+    type_enum = type_mapping.get(type_str.upper())
+
+    if type_enum is None:
+        raise ValueError(f"Invalid type: {type_str}")
+
+    return type_enum
+
+
+class TypeChart:
+    """
+    Class for handling type effectiveness in battles.
+    """
+    def __init__(self):
+        # Initialize type weaknesses, resistances, and immunities
+        self.weakness = {
+            Type.FIRE: [Type.WATER, Type.ROCK, Type.GROUND],
+            Type.WATER: [Type.ELECTRIC, Type.GRASS],
+            Type.ELECTRIC: [Type.GROUND],
+            Type.GRASS: [Type.FIRE, Type.ICE, Type.POISON, Type.FLYING, Type.BUG],
+            Type.ICE: [Type.FIRE, Type.FIGHTING, Type.ROCK, Type.STEEL],
+            Type.FIGHTING: [Type.FLYING, Type.PSYCHIC, Type.FAIRY],
+            Type.POISON: [Type.GROUND, Type.PSYCHIC],
+            Type.GROUND: [Type.WATER, Type.GRASS, Type.ICE],
+            Type.FLYING: [Type.ELECTRIC, Type.ICE, Type.ROCK],
+            Type.PSYCHIC: [Type.BUG, Type.GHOST, Type.DARK],
+            Type.BUG: [Type.FIRE, Type.FLYING, Type.ROCK],
+            Type.ROCK: [Type.WATER, Type.GRASS, Type.FIGHTING, Type.GROUND, Type.STEEL],
+            Type.GHOST: [Type.GHOST, Type.DARK],
+            Type.DRAGON: [Type.ICE, Type.DRAGON, Type.FAIRY],
+            Type.DARK: [Type.FIGHTING, Type.BUG, Type.FAIRY],
+            Type.STEEL: [Type.FIRE, Type.FIGHTING, Type.GROUND],
+            Type.FAIRY: [Type.POISON, Type.STEEL],
+            Type.NORMAL: [Type.FIGHTING]
+        }
+
+        self.resistance = {
+            Type.FIRE: [Type.FIRE, Type.GRASS, Type.ICE, Type.BUG, Type.STEEL, Type.FAIRY],
+            Type.WATER: [Type.WATER, Type.FIRE, Type.ICE, Type.STEEL],
+            Type.ELECTRIC: [Type.ELECTRIC, Type.FLYING, Type.STEEL],
+            Type.GRASS: [Type.WATER, Type.ELECTRIC, Type.GRASS, Type.GROUND],
+            Type.ICE: [Type.ICE],
+            Type.FIGHTING: [Type.BUG, Type.ROCK, Type.DARK],
+            Type.POISON: [Type.GRASS, Type.FIGHTING, Type.POISON, Type.BUG, Type.FAIRY],
+            Type.GROUND: [Type.POISON, Type.ROCK],
+            Type.FLYING: [Type.GRASS, Type.FIGHTING, Type.BUG],
+            Type.PSYCHIC: [Type.FIGHTING, Type.PSYCHIC],
+            Type.BUG: [Type.GRASS, Type.FIGHTING, Type.GROUND],
+            Type.ROCK: [Type.NORMAL, Type.FIRE, Type.POISON, Type.FLYING],
+            Type.GHOST: [Type.POISON, Type.BUG],
+            Type.DRAGON: [Type.FIRE, Type.WATER, Type.ELECTRIC, Type.GRASS],
+            Type.DARK: [Type.GHOST, Type.PSYCHIC, Type.DARK],
+            Type.STEEL: [Type.NORMAL, Type.GRASS, Type.ICE, Type.FLYING, Type.PSYCHIC, Type.BUG, Type.ROCK, Type.DRAGON,
+                         Type.STEEL, Type.FAIRY],
+            Type.FAIRY: [Type.FIGHTING, Type.BUG, Type.DRAGON],
+            Type.NORMAL: []
+        }
+
+        self.immunity = {
+            Type.NORMAL: [Type.GHOST],
+            Type.GROUND: [Type.ELECTRIC],
+            Type.FLYING: [Type.GROUND],
+            Type.DARK: [Type.PSYCHIC],
+            Type.GHOST: [Type.NORMAL, Type.FIGHTING],
+            Type.FAIRY: [Type.DRAGON]
+        }
+
+    def get_weaknesses(self, given_type: Type) -> list[Type]:
+        """
+        Get the weaknesses of a given type.
+
+        Args:
+            given_type (Type): The type to get weaknesses for.
+
+        Returns:
+            list[Type]: A list of types that are weaknesses for the given type.
+        """
+        return self.weakness.get(given_type, [])
+
+    def get_resistances(self, given_type: Type) -> list[Type]:
+        """
+        Get the resistances of a given type.
+
+        Args:
+            given_type (Type): The type to get resistances for.
+
+        Returns:
+            list[Type]: A list of types that are resistances for the given type.
+        """
+        return self.resistance.get(given_type, [])
+
+    def get_immunities(self, given_type: Type) -> list[Type]:
+        """
+        Get the immunities of a given type.
+
+        Args:
+            given_type (Type): The type to get immunities for.
+
+        Returns:
+            list[Type]: A list of types that are immunities for the given type.
+        """
+        return self.immunity.get(given_type, [])
+
+    def get_type_effectiveness(self, attacking_type: Type, defending_type: Type) -> float:
+        """
+        Get the effectiveness of an attacking type against a defending type.
+
+        Args:
+            attacking_type (Type): The type of the attacking move.
+            defending_type (Type): The type of the target Pokemon.
+
+        Returns:
+            float: The effectiveness of the attack (x0, x0.5, x1, or x2).
+        """
+        if defending_type == Type.NORMAL:
+            return 1.0
+
+        if attacking_type in self.get_immunities(defending_type):
+            return 0.0
+        elif attacking_type in self.get_resistances(defending_type):
+            return 0.5
+        elif attacking_type in self.get_weaknesses(defending_type):
+            return 2.0
+        else:
+            return 1.0
---
+++ tests/test_type.py
@@ -0,0 +1,39 @@
+import unittest
+from Pokemon.type import Type, TypeChart, string_to_type
+
+
+class TestTypeChart(unittest.TestCase):
+    def setUp(self):
+        self.type_chart = TypeChart()
+
+    def test_get_weaknesses(self):
+        self.assertEqual(self.type_chart.get_weaknesses(Type.FIRE), [Type.WATER, Type.ROCK, Type.GROUND])
+        self.assertEqual(self.type_chart.get_weaknesses(Type.WATER), [Type.ELECTRIC, Type.GRASS])
+
+    def test_get_resistances(self):
+        self.assertEqual(self.type_chart.get_resistances(Type.FIRE),
+                         [Type.FIRE, Type.GRASS, Type.ICE, Type.BUG, Type.STEEL, Type.FAIRY])
+        self.assertEqual(self.type_chart.get_resistances(Type.WATER), [Type.WATER, Type.FIRE, Type.ICE, Type.STEEL])
+
+    def test_get_immunities(self):
+        self.assertEqual(self.type_chart.get_immunities(Type.NORMAL), [Type.GHOST])
+        self.assertEqual(self.type_chart.get_immunities(Type.GROUND), [Type.ELECTRIC])
+
+    def test_get_type_effectiveness(self):
+        self.assertEqual(self.type_chart.get_type_effectiveness(Type.FIRE, Type.WATER), 0.5)
+        self.assertEqual(self.type_chart.get_type_effectiveness(Type.WATER, Type.FIRE), 2.0)
+        self.assertEqual(self.type_chart.get_type_effectiveness(Type.ELECTRIC, Type.GROUND), 0.0)
+
+    def test_string_to_type_converts(self):
+        self.assertEqual(string_to_type("fire"), Type.FIRE)
+        self.assertEqual(string_to_type("Water"), Type.WATER)
+        self.assertEqual(string_to_type("BUG"), Type.BUG)
+
+    def test_string_to_type_throws(self):
+        with self.assertRaises(ValueError):
+            string_to_type("bla")
+            string_to_type("")
+
+
+if __name__ == '__main__':
+    unittest.main()
